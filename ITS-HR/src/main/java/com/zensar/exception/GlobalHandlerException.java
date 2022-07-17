43cd541b462e5726eb15bc9101c0deba8ab3716d
satyamrakhonde <76210917+satyamrakhonde@users.noreply.github.com>
2022-07-17 14:27:55 +0530

    Add files via upload

---
+++ ITS-HR/src/main/java/com/zensar/exception/GlobalHandlerException.java
@@ -0,0 +1,31 @@
+package com.zensar.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class GlobalHandlerException extends ResponseEntityExceptionHandler {
+	
+	//@ControllerAdvice
+	@ExceptionHandler(value=InvalidAuthTokenException.class)
+	public ResponseEntity<Object> handleConflict1(RuntimeException exception, WebRequest request){
+		String errorMessage= "{\"error\":Invalid token\"}";
+		ResponseEntity<Object> response=
+				handleExceptionInternal(exception, errorMessage, new HttpHeaders(), HttpStatus.CONFLICT, request);
+		return response;
+	}
+	
+	@ExceptionHandler(value = InvalidIdException.class)
+	public ResponseEntity<Object> handleConflictInvalidAdvertiseID(RuntimeException exception , WebRequest request){
+	String errorMessage="{\"error\":"+exception.toString() + " \"}";
+	ResponseEntity<Object> response=
+	handleExceptionInternal(exception,errorMessage,new HttpHeaders(),HttpStatus.CONFLICT, request);
+	return response;
+	}
+	
+
+}
---
+++ ITS-HR/src/main/java/com/zensar/service/AdminServiceDelegate.java
@@ -0,0 +1,6 @@
+package com.zensar.service;
+
+public interface AdminServiceDelegate {
+	public boolean isDeleteSuccessful(int id);
+
+}
